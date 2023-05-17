4f1d437adec0ac1f4fd1737abc15ccf1c207aafa
Jongwoo Choi <muvnelik@naver.com>
2023-05-17 19:30:56 +0900

    app.py 메인화면 main.html로 변경

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from bs4 import BeautifulSoup
 
 @app.route('/')
 def home():
-    return render_template('teamcard.html') 
+    return render_template('main.html') 
 # 우선 메인페이지 연결함
 
 @app.route('/page2')
