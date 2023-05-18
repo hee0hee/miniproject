728262e299d3b5eaa514d0926e407229f69e02ca
ayoung <ayoung3052@gmail.com>
2023-05-18 10:19:10 +0900

    연결 관리

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\muvne\OneDrive\문서\GitHub\miniproject\venv
+set VIRTUAL_ENV=C:\Users\USER\Desktop\sparta\miniproject\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
